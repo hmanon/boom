e03bfcd1bda099b6c9c938544c1e049c2e819548
hm <hmail@list.ru>
2012-06-15 00:07:27 +0400

    init

---
+++ front/commons/src/main/java/com/boom/front/commons/App.java
@@ -0,0 +1,13 @@
+package com.boom.front.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
